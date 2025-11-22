f0bb12fcc4a029db13770dcb257c51ebc32a5588
dietken1 <dietken1@ajou.ac.kr>
2025-11-23 04:05:15 +0900

    [Feat] ENUM 수정

--- src/main/java/com/soptkathonserverandroid2/domain/issue/entity/enums/Range.java
+++ src/main/java/com/soptkathonserverandroid2/domain/issue/entity/enums/Range.java
@@ -1,5 +1,10 @@
 package com.soptkathonserverandroid2.domain.issue.entity.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Range {
     ALL("전체"),
     DEPARTMENT("학과"),
